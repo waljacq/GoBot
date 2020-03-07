aec03a6e593ed58ff7f125d7a75fbccaacb1314c
Walter Jacquette <waltjacquette@gmail.com>
2020-03-07 15:43:04 -0500

    New Discord Package

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"go-bot/pkg/discord"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	if os.Getenv("BOT_AUTH") != "" {
+		fmt.Println("Please set your 'BOT_AUTH' environment Variable")
+		os.Exit(1)
+	}
+	discord.New()
 }
---
+++ pkg/discord/discord.go
@@ -0,0 +1,7 @@
+package discord
+
+import "fmt"
+
+func New() {
+	fmt.Println("New Discord Connection")
+}
