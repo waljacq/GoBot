0dbb72d0d34521726f8b99272e881df27c79fdfc
Walter Jacquette <waltjacquette@gmail.com>
2020-05-23 16:41:34 -0500

    Update CMDS map

--- pkg/commands/commands.go
+++ pkg/commands/commands.go
@@ -9,21 +9,23 @@ import (
 
 // CMDS ...
 var CMDS = map[string]func(msg Message){
-	"ping": func(msg Message) {
-		data := []byte(`{"content": "Pong!"}`)
-		client := &http.Client{}
-		url := fmt.Sprintf("https://discordapp.com/api/v6/channels/%s/messages", msg.ChannelID)
-		body := bytes.NewBuffer(data)
-		request, err := http.NewRequest("POST", url, body)
-		if err != nil {
-			panic(err)
-		}
-		request.Header.Add("Authorization", fmt.Sprintf("Bot %s", os.Getenv("BOT_AUTH")))
-		request.Header.Add("Content-Type", "application/json")
-		resp, err := client.Do(request)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(resp)
-	},
+	"ping": ping,
+}
+
+func ping(msg Message) {
+	data := []byte(`{"content": "Pong!"}`)
+	client := &http.Client{}
+	url := fmt.Sprintf("https://discordapp.com/api/v6/channels/%s/messages", msg.ChannelID)
+	body := bytes.NewBuffer(data)
+	request, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Add("Authorization", fmt.Sprintf("Bot %s", os.Getenv("BOT_AUTH")))
+	request.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp)
 }
